3a8190b2f53ec09687b9f30b910b95cd2dcadd96
Silver Samma <silver.samma@gmail.com>
2015-12-15 13:16:24 +0200

    Calc.JUNIOR jne kasutuses, emplee valued klassimuutujateks

--- src/ee/tty/praktikum2/Calc.java
+++ src/ee/tty/praktikum2/Calc.java
@@ -1,9 +1,5 @@
 package ee.tty.praktikum2;
 
-import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 //represents reusable domain service which calculates daily salaries
 public class Calc {
     //employee types
@@ -11,81 +7,60 @@ public class Calc {
     public static final int SENIOR = 2;
     public static final int SPECIALIST = 3;
     
-    static final Map<Integer, Integer> MONEY_PER_HOUR;
-    static {
-    	Map<Integer, Integer> tmp = new LinkedHashMap<Integer, Integer>();
-    	tmp.put(JUNIOR, 10);
-    	tmp.put(SENIOR, 15);
-    	tmp.put(SPECIALIST, 22);
-    	MONEY_PER_HOUR = Collections.unmodifiableMap(tmp);
-    }
+	private int moneyPerHour;
+	private int normalHours;
+	private int extraHoursMultiplier;
+	private int heroBonus;
+	private int extremeHours = 20;
+	
+	private void setEmploeeValues(int type){
+    	if (type == JUNIOR) {
+    		moneyPerHour = 10;
+    		normalHours = 8;
+    		extraHoursMultiplier = 2;
+    		heroBonus = 10;
+    	}
+    	if (type == SENIOR) {
+    		moneyPerHour = 15;
+    		normalHours = 8;
+    		extraHoursMultiplier = 2;
+    		heroBonus = 20;
+    	}
+    	if (type == SPECIALIST) {
+    		moneyPerHour = 22;
+    		normalHours = 9;
+    		extraHoursMultiplier = 3;
+    		heroBonus = 30;
+    	}
+	}
 
-    static final Map<Integer, Integer> NORMAL_HOURS;
-    static {
-    	Map<Integer, Integer> tmp = new LinkedHashMap<Integer, Integer>();
-    	tmp.put(JUNIOR, 8);
-    	tmp.put(SENIOR, 8);
-    	tmp.put(SPECIALIST, 9);
-    	NORMAL_HOURS = Collections.unmodifiableMap(tmp);
-    }
-
-    static final Map<Integer, Integer> EXTRA_HOURS_MULTIPLIER;
-    static {
-    	Map<Integer, Integer> tmp = new LinkedHashMap<Integer, Integer>();
-    	tmp.put(JUNIOR, 2);
-    	tmp.put(SENIOR, 2);
-    	tmp.put(SPECIALIST, 3);
-    	EXTRA_HOURS_MULTIPLIER = Collections.unmodifiableMap(tmp);
-    }
-
-    static final Map<Integer, Integer> HERO_BONUS;
-    static {
-    	Map<Integer, Integer> tmp = new LinkedHashMap<Integer, Integer>();
-    	tmp.put(JUNIOR, 10);
-    	tmp.put(SENIOR, 20);
-    	tmp.put(SPECIALIST, 30);
-    	HERO_BONUS = Collections.unmodifiableMap(tmp);
-    }
-    
-    public static final int EXTREME_HOURS = 20;
-
-
-
+	
     protected int pay(final int type,final int hours) {
-        int moneyPerHour = MONEY_PER_HOUR.get(type);
-        int normalHours = NORMAL_HOURS.get(type);
-        int extraHoursMultiplier = EXTRA_HOURS_MULTIPLIER.get(type);
-        int heroBonus = HERO_BONUS.get(type);
-    	int extremeHours = EXTREME_HOURS;
-    	
-    	return calculatePay(hours, moneyPerHour, normalHours, extraHoursMultiplier, heroBonus, extremeHours);
+    	setEmploeeValues(type);
+    	int Sum = 0;
+
+    	Sum += calulateNormalHoursPay(hours);
+		Sum += calculateExtraHoursPay(hours);
+        Sum += calculateHeroBonus(hours);
+
+        return Sum;
     }
     
-	private int calulateNormalHoursPay(int hours, int normalHours, int moneyPerHour){
+	private int calulateNormalHoursPay(int hours){
 		return Math.min(hours, normalHours) * moneyPerHour;
 	}
 
-	private int calculateExtraHoursPay(int hours, int normalHours, int moneyPerHour, int extraHoursMultiplier){
+	private int calculateExtraHoursPay(int hours){
     	if (hours > normalHours){
     		return (hours - normalHours) * moneyPerHour * extraHoursMultiplier;
     	}
     	return 0;
 	}
 	
-	private int calculateHeroBonus(int hours, int extremeHours, int heroBonus){
+	private int calculateHeroBonus(int hours){
         if (hours > extremeHours) {
             return heroBonus;
         }
         return 0;
-    }
-	
-    private int calculatePay(int hours, int moneyPerHour, int normalHours, int extraHoursMultiplier, int heroBonus, int extremeHours){
-    	int Sum = 0;
-
-    	Sum += calulateNormalHoursPay(hours, normalHours, moneyPerHour);
-		Sum += calculateExtraHoursPay(hours, normalHours, moneyPerHour, extraHoursMultiplier);
-        Sum += calculateHeroBonus(hours, extremeHours, heroBonus);
-
-        return Sum;
     }
 }
--- src/ee/tty/praktikum2/TestCalc.java
+++ src/ee/tty/praktikum2/TestCalc.java
@@ -7,10 +7,6 @@ import org.junit.Test;
 
 public class TestCalc {
 	private Calc calc;
-
-    public static final int JUNIOR = 1;
-    public static final int SENIOR = 2;
-    public static final int SPECIALIST = 3;
     
 	@Before
 	public void creatCalc(){
@@ -19,37 +15,32 @@ public class TestCalc {
 
 	@Test
 	public void testPayJuniorZero() {
-		int type = JUNIOR;
 		int hours = 0;
-		assertEquals(0, calc.pay(type, hours));
+		assertEquals(0, calc.pay(Calc.JUNIOR, hours));
 	}
 
 	@Test
 	public void testPayJuniorNormal() {
-		int type = JUNIOR;
 		int hours = 6;
-		assertEquals(60, calc.pay(type, hours));
+		assertEquals(60, calc.pay(Calc.JUNIOR, hours));
 	}
 
 	@Test
 	public void testPayJunior8() {
-		int type = JUNIOR;
 		int hours = 8;
-		assertEquals(80, calc.pay(type, hours));
+		assertEquals(80, calc.pay(Calc.JUNIOR, hours));
 	}
 
 	@Test
 	public void testPayJunior8Plus() {
-		int type = JUNIOR;
 		int hours = 9;
-		assertEquals(100, calc.pay(type, hours));
+		assertEquals(100, calc.pay(Calc.JUNIOR, hours));
 	}
 
 	@Test
 	public void testPayJunior20Plus() {
-		int type = JUNIOR;
 		int hours = 21;
-		assertEquals(350, calc.pay(type, hours));
+		assertEquals(350, calc.pay(Calc.JUNIOR, hours));
 	}
 	
 	// SENIOR
@@ -57,80 +48,69 @@ public class TestCalc {
 	
 	@Test
 	public void testPaySeniorZero() {
-		int type = SENIOR;
 		int hours = 0;
-		assertEquals(0, calc.pay(type, hours));
+		assertEquals(0, calc.pay(Calc.SENIOR, hours));
 	}
 
 	@Test
 	public void testPaySeniorNormal() {
-		int type = SENIOR;
 		int hours = 6;
-		assertEquals(90, calc.pay(type, hours));
+		assertEquals(90, calc.pay(Calc.SENIOR, hours));
 	}
 
 	@Test
 	public void testPaySenior8() {
-		int type = SENIOR;
 		int hours = 8;
-		assertEquals(120, calc.pay(type, hours));
+		assertEquals(120, calc.pay(Calc.SENIOR, hours));
 	}
 
 	@Test
 	public void testPaySenior8Plus() {
-		int type = SENIOR;
 		int hours = 9;
-		assertEquals(150, calc.pay(type, hours));
+		assertEquals(150, calc.pay(Calc.SENIOR, hours));
 	}
 
 	@Test
 	public void testPaySenior20Plus() {
-		int type = SENIOR;
 		int hours = 21;
-		assertEquals(530, calc.pay(type, hours));
+		assertEquals(530, calc.pay(Calc.SENIOR, hours));
 	}	
 	
 	// SPECIALIST
 	
 	@Test
 	public void testPaySpecialistZero() {
-		int type = SPECIALIST;
 		int hours = 0;
-		assertEquals(0, calc.pay(type, hours));
+		assertEquals(0, calc.pay(Calc.SPECIALIST, hours));
 	}
 
 	@Test
 	public void testPaySpecialistNormal() {
-		int type = SPECIALIST;
 		int hours = 6;
-		assertEquals(132, calc.pay(type, hours));
+		assertEquals(132, calc.pay(Calc.SPECIALIST, hours));
 	}
 
 	@Test
 	public void testPaySpecialist8() {
-		int type = SPECIALIST;
 		int hours = 8;
-		assertEquals(176, calc.pay(type, hours));
+		assertEquals(176, calc.pay(Calc.SPECIALIST, hours));
 	}
 
 	@Test
 	public void testPaySpecialist9() {
-		int type = SPECIALIST;
 		int hours = 9;
-		assertEquals(198, calc.pay(type, hours));
+		assertEquals(198, calc.pay(Calc.SPECIALIST, hours));
 	}
 
 	@Test
 	public void testPaySpecialist9Plus() {
-		int type = SPECIALIST;
 		int hours = 10;
-		assertEquals(264, calc.pay(type, hours));
+		assertEquals(264, calc.pay(Calc.SPECIALIST, hours));
 	}
 
 	@Test
 	public void testPaySpecialist20Plus() {
-		int type = SPECIALIST;
 		int hours = 21;
-		assertEquals(1020, calc.pay(type, hours));
+		assertEquals(1020, calc.pay(Calc.SPECIALIST, hours));
 	}		
 }
